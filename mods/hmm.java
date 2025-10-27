b448d9e7317e13b60e6849f237d0e11ca0e14718
LunarMoonDLCT <levinhtoan8116pc@gmail.com>
2025-10-27 19:41:05 +0700

    Add files via upload

---
+++ mods/hmm.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class hmm {
+    public static void main(String[] args) {
+        // Tạo một cửa sổ JFrame
+        JFrame frame = new JFrame("ngủ");
+        
+        // Đặt kích thước cửa sổ
+        frame.setSize(300, 200);
+        
+        // Đặt chế độ thoát khi đóng cửa sổ
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Hiển thị cửa sổ
+        frame.setVisible(true);
+    }
+}
